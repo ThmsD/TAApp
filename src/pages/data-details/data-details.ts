edf00b1ea346dc0bad3345563ee88c8ef9475beb
Thomas D <thomas.dressel@iisys.de>
2018-01-16 16:23:23 +0100

    swipeable tabs added to the details page

--- src/pages/data-details/data-details.ts
+++ src/pages/data-details/data-details.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -7,12 +7,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'data-details.html',
 })
 export class DataDetailsPage {
+  //------------------------------
+  @ViewChild('SwipedTabsSlider') SwipedTabsSlider: Slides ;
+
+  SwipedTabsIndicator :any= null;
+  tabs:any=[];
+
+  //------------------------------
 
   private item: any;
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.loadData();
+    this.tabs=["page1","page2","page3","page4"];
   }
 
   ionViewDidLoad() {
@@ -24,6 +32,32 @@ export class DataDetailsPage {
     this.item = JSON.parse(JSON.stringify(this.navParams.get("item")));
     //this.item = JSON.parse(temp);
     console.log("item: " + (this.item.name));
+  }
+
+
+
+
+  ionViewDidEnter() {
+    this.SwipedTabsIndicator = document.getElementById("indicator");
+  }
+
+   selectTab(index) {    
+    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(100*index)+'%,0,0)';
+    this.SwipedTabsSlider.slideTo(index, 500);
+  }
+
+  updateIndicatorPosition() {
+      // this condition is to avoid passing to incorrect index
+  	if( this.SwipedTabsSlider.length()> this.SwipedTabsSlider.getActiveIndex())
+  	{
+  		this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(this.SwipedTabsSlider.getActiveIndex() * 100)+'%,0,0)';
+  	}
+    
+    }
+
+  animateIndicator($event) {
+  	if(this.SwipedTabsIndicator)
+   	    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (($event.progress* (this.SwipedTabsSlider.length()-1))*100) + '%,0,0)';
   }
 
 }
