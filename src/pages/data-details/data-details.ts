670ceb3b9682943aa3f81fe0dd895edc76f036f9
ThmsD <thomas.dressel@uni-weimar.de>
2018-01-18 18:51:35 +0100

    chart basics added and styled

--- src/pages/data-details/data-details.ts
+++ src/pages/data-details/data-details.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
+import { Chart } from "chart.js";
 
 @IonicPage()
 @Component({
@@ -7,24 +8,73 @@ import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
   templateUrl: 'data-details.html',
 })
 export class DataDetailsPage {
+  @ViewChild('lineCanvas') lineCanvas;
   //------------------------------
-  @ViewChild('SwipedTabsSlider') SwipedTabsSlider: Slides ;
+  @ViewChild('SwipedTabsSlider') SwipedTabsSlider: Slides;
 
-  SwipedTabsIndicator :any= null;
-  tabs:any=[];
+  SwipedTabsIndicator: any = null;
+  tabs: any = [];
 
   //------------------------------
 
   private item: any;
+  lineChart: any;
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.loadData();
-    this.tabs=["page1","page2","page3","page4"];
+    this.tabs = ["Tag", "Woche", "Monat", "Jahr"];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DataDetailsPage');
+
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+
+      type: 'line',
+      data: {
+        labels: ["01:00", "02:00", "03:00", "04:00", "05:00", "06:00", 
+                "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", 
+                "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", 
+                "19:00", "20:00", "21:00", "22:00", "23:00", "00:00"],
+        datasets: [
+          {
+            label: "kW",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [0, 0, 0, 0, 0, 0, 1.2, 1.8, 2.3, 3.5, 5.1, 6.7, 6.5, 5.3, 5, 4.8],
+            spanGaps: false,
+          }
+        ]
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+
+    });
   }
 
   loadData() {
@@ -36,28 +86,31 @@ export class DataDetailsPage {
 
 
 
+  //------------------------------
 
   ionViewDidEnter() {
     this.SwipedTabsIndicator = document.getElementById("indicator");
   }
 
-   selectTab(index) {    
-    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(100*index)+'%,0,0)';
+  selectTab(index) {
+    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (100 * index) + '%,0,0)';
     this.SwipedTabsSlider.slideTo(index, 500);
   }
 
   updateIndicatorPosition() {
-      // this condition is to avoid passing to incorrect index
-  	if( this.SwipedTabsSlider.length()> this.SwipedTabsSlider.getActiveIndex())
-  	{
-  		this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(this.SwipedTabsSlider.getActiveIndex() * 100)+'%,0,0)';
-  	}
-    
+    // this condition is to avoid passing to incorrect index
+    if (this.SwipedTabsSlider.length() > this.SwipedTabsSlider.getActiveIndex()) {
+      this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (this.SwipedTabsSlider.getActiveIndex() * 100) + '%,0,0)';
     }
 
-  animateIndicator($event) {
-  	if(this.SwipedTabsIndicator)
-   	    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (($event.progress* (this.SwipedTabsSlider.length()-1))*100) + '%,0,0)';
   }
+
+  animateIndicator($event) {
+    if (this.SwipedTabsIndicator)
+      this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (($event.progress * (this.SwipedTabsSlider.length() - 1)) * 100) + '%,0,0)';
+  }
+
+  //------------------------------
+
 
 }
